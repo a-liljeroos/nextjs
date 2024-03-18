fb65c2288e787b6895df816331643bfecf84fa22
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-03-18 13:35:38 +0200

    redux setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
-import Image from "next/image";
+"use client";
+// redux
+import { Provider } from "react-redux";
+import { store } from "./_lib/store";
 // components
 import GlitchText from "./_components/glitchText/GlitchText";
 import ScreenGlitch from "./_components/screenGlitch/ScreenGlitch";
+import NavMenu from "./_components/navMenu/NavMenu";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-2 items-left justify-start relative">
-      <nav></nav>
-      <section className="section-1 relative">
-        <ScreenGlitch />
-        <div className="glitch-text-cont">
-          <GlitchText text="      " />
-          <GlitchText text="Hi  " />
-          <GlitchText text="There " />
-          <GlitchText text=" " />
-          <GlitchText text="      " />
-        </div>
-      </section>
-    </main>
+    <Provider store={store}>
+      <main className="flex min-h-screen flex-col px-2 items-left justify-start relative">
+        <NavMenu />
+        <section className="section-1 relative">
+          <ScreenGlitch />
+          <div className="glitch-text-cont">
+            <GlitchText text="      " />
+            <GlitchText text="Hi  " />
+            <GlitchText text="There " />
+            <GlitchText text=" " />
+            <GlitchText text="      " />
+          </div>
+        </section>
+      </main>
+    </Provider>
   );
 }
