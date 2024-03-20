6afd35681081e90a67ba10174e2266b132eeb464
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-03-20 14:43:01 +0200

    removed imports and comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React from "react";
 // redux
 import { Provider } from "react-redux";
 import { store } from "./_lib/store";
 // components
-import GlitchText from "./_components/glitchText/GlitchText";
-import ScreenGlitch from "./_components/screenGlitch/ScreenGlitch";
 import NavMenu from "./_components/navMenu/NavMenu";
 import Section2 from "./_components/HomePage/Section2";
 import Section1 from "./_components/HomePage/Section1";
 
 export default function Home() {
-  // when the next section is 10% in the viewport, scroll to it
-
   return (
     <Provider store={store}>
       <main className="flex min-h-screen flex-col items-left justify-start relative">
