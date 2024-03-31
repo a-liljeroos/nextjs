29ae0a5a49f0ab5c2037a0337ac818ac7e18b4b7
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-03-31 18:28:55 +0300

    login, register button, user image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+Date.prototype.addHours = function (h) {
+  this.setTime(this.getTime() + h * 60 * 60 * 1000);
+  return this;
+};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/_components/navMenu/NavMenu.tsx
+++ src/app/_components/navMenu/NavMenu.tsx
@@ -1,12 +1,19 @@
 import React, { useRef } from "react";
+import Image from "next/image";
+// auth
+import { useSession, signIn, signOut } from "next-auth/react";
 // redux
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../_lib/store";
 import { toggleMenu } from "../../_lib/menuSlice";
 //styles
 import "./navMenu.scss";
+import Link from "next/link";
 
 const NavMenu = () => {
+  // auth
+  const { data: session } = useSession();
+
   // menu reducer
   const isOpen = useSelector((state: RootState) => state.menu.isOpen);
   const dispatch = useDispatch();
@@ -18,7 +25,7 @@ const NavMenu = () => {
   const toggleBoard = (isOpen: boolean): void => {
     let opeKeyFrames = new KeyframeEffect(
       navBoardRef.current,
-      [{ right: "-115%" }, { right: "-30px" }],
+      [{ right: "-100vw" }, { right: "-30px" }],
       {
         duration: 250,
         easing: "ease-in-out",
@@ -29,7 +36,7 @@ const NavMenu = () => {
 
     let closeKeyFrames = new KeyframeEffect(
       navBoardRef.current,
-      [{ right: "-30px" }, { right: "-115%" }],
+      [{ right: "-30px" }, { right: "-100vw" }],
       {
         duration: 250,
         easing: "ease-in-out",
@@ -70,18 +77,46 @@ const NavMenu = () => {
     <nav>
       <button
         ref={toggleButtonRef}
-        className="nav-btn"
+        className="menu-btn"
         onClick={() => {
           dispatch(toggleMenu());
           toggleBoard(isOpen);
         }}
       >
-        🤪
+        👆
       </button>
+      {session ? (
+        <div className="nav-session-data">
+            {session.user?.image && (
+              <Image
+              src={session.user?.image as string}
+              alt={session.user?.name as string}
+              width="60"
+              height="60"
+              style={{ borderRadius: "50%" }}
+              />
+            )}
+          <button onClick={() => signOut()} className="nav-btn">
+            Logout
+          </button>
+        </div>
+      ) : (
+          <div className="flex gap-2">
+           <button onClick={() => signIn()} className="nav-btn">
+              Login
+          </button>
+          <Link href="/register">
+            <button className="nav-btn">
+               Register
+            </button>
+           </Link>
+          </div>
+      )}
+
       <div className="relative">
         <div
           ref={navBoardRef}
-          style={{ position: "absolute", top: "30", right: "-115%" }}
+          style={{ position: "absolute", top: "30", right: "-100vw" }}
           className="nav-board"
         >
           <div className="nav-board-content">
