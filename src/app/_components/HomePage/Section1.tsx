fedeaf8d5d26c4bc92a3b699701255f9c6a95b42
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-03-31 19:57:51 +0300

    show user data in the home page

--- src/app/_components/HomePage/Section1.tsx
+++ src/app/_components/HomePage/Section1.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// auth
+import { useSession} from "next-auth/react";
 // components
 import Section from "./Section";
 import ScreenGlitch from "../screenGlitch/ScreenGlitch";
@@ -6,10 +8,13 @@ import GlitchText from "../glitchText/GlitchText";
 
 interface Section1Props {}
 
-const Section1 = ({}: Section1Props) => {
+const Section1 = ({ }: Section1Props) => {
+  const { data: session } = useSession();
+
   return (
     <Section id="section-1" className="section-1">
-      <ScreenGlitch />
+      <ScreenGlitch />{session && <div className="absolute left-4 top-28 opacity-30 text-xs user-select-none"><pre>{JSON.stringify(session, null, 2) }</pre></div>}
+      
       <div className="glitch-text-cont">
         <GlitchText text="      " />
         <GlitchText text="Hi  " />
