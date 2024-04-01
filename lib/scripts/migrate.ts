df356844f8195a323aac4398d477f2b88a3cb7e2
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-04-01 22:46:28 +0300

    migrate.ts add disconnection statement

--- lib/scripts/migrate.ts
+++ lib/scripts/migrate.ts
@@ -1,8 +1,11 @@
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 import { db } from "../db";
+import { sql } from "@vercel/postgres";
 
 async function main() {
   await migrate(db, { migrationsFolder: "./drizzle" });
+  await sql.end();
+  console.log("Migration complete");
 }
 
 main();
