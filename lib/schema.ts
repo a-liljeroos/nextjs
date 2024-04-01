e9b4e3e489b67f0661c00a7ebe617a2394ceec9d
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-04-01 22:35:20 +0300

    email column to the users table

--- lib/schema.ts
+++ lib/schema.ts
@@ -11,6 +11,7 @@ export const users = pgTable(
   {
     id: serial("id").primaryKey(),
     username: varchar("username", { length: 256 }).notNull(),
+    email: varchar("email", { length: 256 }),
     password: varchar("password", { length: 256 }).notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at"),
